ccab6f56e256fddd4f26e1b40b2e11f4b0c43934
Tim Solley <tsolley@vergeops.com>
2019-07-29 19:49:35 -0700

    Updated VPC id

--- terraform/security_group/main.tf
+++ terraform/security_group/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_security_group" "security_group_name" {
   name        = "security_group_name"
   description = "Allow port 8000"
-  vpc_id      = "vpc-594fdb23"
+  vpc_id      = "vpc-b51869cf"
 
   ingress {
     from_port   = 8000
